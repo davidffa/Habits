39e8bb4437fb883b69dda459a03a51bf424e8179
David Amorim <davidfilipeamorim@gmail.com>
2023-01-18 17:53:27 +0000

    Add day details route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { prisma } from './lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/habits', async (req, res) => {
+  app.post('/habits', async (req) => {
     const createHabitBody = z.object({
       title: z.string(),
       weekDays: z.array(
@@ -30,4 +30,46 @@ export async function appRoutes(app: FastifyInstance) {
       }
     })
   });
+
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const parsedDate = dayjs(date).startOf('day');
+    const weekDay = parsedDate.day();
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits
+    };
+  })
 }
